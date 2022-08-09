6d52ce60b88f1c107e0ff3974fc67b8b1278f571
Richard Ma <richard.ma.19850509@gmail.com>
2022-08-09 22:10:57 +0800

    init database

---
+++ data/init_db.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS book;
+CREATE TABLE book (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    author TEXT,
+    publisher  TEXT,
+    isbn TEXT,
+    image TEXT,
+    flag TEXT
+);
+
+DROP TABLE IF EXISTS audience;
+CREATE TABLE audience(
+    id TEXT PRIMARY KEY,
+    password TEXT
+);
+
+DROP TABLE IF EXISTS store;
+CREATE TABLE store (
+    id TEXT PRIMARY KEY,
+    book_id TEXT REFERENCES book(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    status TEXT,
+    flag TEXT
+);
+
+DROP TABLE IF EXISTS borrow;
+CREATE TABLE borrow (
+    id TEXT PRIMARY KEY,
+    store_id TEXT REFERENCES store(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    audience_id TEXT REFERENCES audience(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
